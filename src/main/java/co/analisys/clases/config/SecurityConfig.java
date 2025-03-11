6e71e60c5c5eea35a9f84930aad5b137927f07ab
Dantesiio <DavidDo29@hotmail.com>
2025-03-11 15:18:17 -0500

    feat: config openApi swagger

--- src/main/java/co/analisys/clases/config/SecurityConfig.java
+++ src/main/java/co/analisys/clases/config/SecurityConfig.java
@@ -18,7 +18,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/circulacion/public/**").permitAll()
+                        .requestMatchers(
+                                "/v3/api-docs/**",  // Documentación OpenAPI
+                                "/swagger-ui/**",    // Interfaz Swagger
+                                "/swagger-ui.html",  // Página principal Swagger
+                                "/circulacion/public/**"
+                        ).permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt ->
